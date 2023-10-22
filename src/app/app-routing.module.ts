f9fbcf0d5751feb3bb26dbdca0627ac6cdd5623f
sgaidhani63 <sgaidhani63@gmail.com>
2023-10-22 08:04:28 +0530

    Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SampleFormComponent } from './sample-form/sample-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+const routes: Routes= [
+ 
+  { path:'', component: LoginFormComponent},
+   {path: 'sample-form', loadChildren:()=>import ('./sample-form/sample-form.component').then(mod=>mod.SampleFormComponent)},
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MainModule } from "./main/main.module";
+import { AppRoutingModule } from './app-routing.module'; 
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent,],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
